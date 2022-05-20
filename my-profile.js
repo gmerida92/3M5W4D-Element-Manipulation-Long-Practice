07a4b8400f345fedffa30b63ab5dbbcf6d89fca7
George Merida <gmerida92@gmail.com>
2022-05-19 21:27:54 -0700

    Phase 2 Commit

--- my-profile.js
+++ my-profile.js
@@ -1,9 +1,10 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+
   console.log('DOM fully loaded and parsed');
   const h1 = document.createElement("h1")
   h1.innerText = "Potato Head"
   // h1.id = "potato-head"
-  h1.setAttribute("id", "potato-head" )
+  h1.setAttribute("id", "potato-head")
   document.body.appendChild(h1);
 
   const div = document.createElement("div");
@@ -13,35 +14,62 @@ window.addEventListener('DOMContentLoaded', (event) => {
   document.body.appendChild(div);
 
   const list = document.createElement("ul");
-  list.innerHTML = '<li>Age: 100</li>' + '<li>Location: Los Angeles</li>' + '<li>Occupation: Actor</li>' + '<li>Favorite Companion: Mrs. Potato Head</li>'
+  // list.innerHTML = '<li>Age: 100</li>' + '<li>Location: Los Angeles</li>' + '<li>Occupation: Actor</li>' + '<li>Favorite Companion: Mrs. Potato Head</li>'
   document.body.appendChild(list)
 
   // make an array with strings to place in list items
-  let listItems = ['Age: 100', 'Location: Los Angeles', 'Occupation: Actor', 'Favorite Companion: Mrs. Potato Head', 'Favorite Movie: Toy Story', 'Favorite Ice Cream: Chocolate']
+  let listItems = ['Age: 100', 'Location: Los Angeles', 'Occupation: Actor', 'Favorite Companion: Mrs. Potato Head', 'Favorite Movie: Toy Story', 'Favorite Ice Cream: Chocolate',"Social-Security: xxx-xx-8989"]
+  
+  // for (let i = 0; i < listItems.length; i++) {
+  //   let newListItem = document.createElement("li");
+  //   newListItem.innerText = list[i];
+  //   document.querySelector("ul").appendChild(newListItem);
+  // }
+  
+  
+  addToPersonalList(listItems);
 
-  // length of ul element
-  let ulLength = document.querySelector('ul').childNodes.length
+  // // length of ul element
+  // let ulLength = document.querySelector('ul').childNodes.length
+  // console.log(typeof ulLength);
 
-  // compare number of items in listItems array against the number of items in
-  // the ul element
-  if (listItems.length > ulLength) {
-    // create a new li element
-    let newItem = document.createElement("li");
-    list.innerHtml += `${<li></li>}`
-    // set innerText
-    newItem.innerText = listItems[listItems.length - 1];
+  // // compare number of items in listItems array against the number of items in
+  // // the ul element
+  // if (listItems.length > ulLength) {
+  //   // create a new li element
+  //   let newItem = document.createElement("li");
+  //   // list.innerHtml += "<li>"+`${listItems[listItems.length - 1]}`+"</li>"
 
-    // append new item to ul
+  //   // set innerText
+  //   newItem.innerText = listItems[listItems.length - 1];
+  //   console.log(newItem)
+  //   console.log(list)
 
-  }
+  //   // append new item to ul
+  //   document.querySelector("ul").appendChild(newItem)
+  
+  // Creates list element
+  // Passes in the string as the innerText
+  // Appends new element to the ul
 
 
+  h1.setAttribute("class", "person-name");
+  list.setAttribute("class", "my-details");
+  document.body.querySelectorAll('li').forEach(el => el.setAttribute("class", "detail"));
+  
 
+
+  
+  
+  
+  
 });
 
-// Creates list element
-// Passes in the string as the innerText
-// Appends new element to the ul
-function addToPersonalList() {
 
+function addToPersonalList(listArray) {
+  for (let i = 0; i < listArray.length; i++) {
+    let newListItem = document.createElement("li");
+    newListItem.innerText = listArray[i];
+    document.querySelector("ul").appendChild(newListItem);
+  }
 }
